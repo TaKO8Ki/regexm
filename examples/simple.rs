d44f15fdb315617ea9dfcbf34701389139948446
Takayuki Maeda <takoyaki0316@gmail.com>
2021-01-09 17:37:24 +0900

    add let match

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,7 +1,16 @@
 fn main() {
-    let text = "2014-01-01";
+    let text = "2020-01-01";
     regexm::regexm!(match text {
         r"^\d{4}-\d{2}-\d{2}$" => println!("ymd"),
         r"^\d{4}-\d{2}$" => println!("ym"),
-    })
+        _ => println!("default"),
+    });
+
+    let text2 = "foo";
+    regexm::regexm!(let foo = match text2 {
+        r"^\d{4}-\d{2}-\d{2}$" => "ymd",
+        r"^\d{4}-\d{2}$" => "ym",
+        _ => "default"
+    });
+    println!("{}", foo)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #[macro_export]
 macro_rules! regexm {
     ($($tokens:tt)*) => {
-        $crate::__regexm! {
+        $crate::regexm_parse! {
             tokens = [$($tokens)*],
         }
     };
@@ -9,20 +9,52 @@ macro_rules! regexm {
 
 #[macro_export]
 #[doc(hidden)]
-macro_rules! __regexm {
+macro_rules! regexm_parse {
     (
-        tokens = [match $str:tt {$pattern:expr => $tokens:expr, $($rest:tt)*}],
+        tokens = [let $var:tt = match $str:tt {$pattern:expr => $token:expr, $($rest:tt)*}],
     ) => {
-        $crate::__regexm! {
+        $crate::__regexm_parse! {
             tokens = [$($rest)*],
             str = $str,
             first_pattern = $pattern,
-            first_token = $tokens,
+            first_token = $token,
+            var = $var,
         }
     };
 
     (
-        tokens = [$($pattern:expr => $tokens:expr$(,)?)*],
+        tokens = [match $str:tt {$pattern:expr => $token:expr, $($rest:tt)*}],
+    ) => {
+        $crate::__regexm_parse! {
+            tokens = [$($rest)*],
+            str = $str,
+            first_pattern = $pattern,
+            first_token = $token,
+        }
+    };
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! __regexm_parse {
+    (
+        tokens = [$($pattern:expr => $tokens:expr,)* _ => $default_token:expr$(,)?],
+        str = $str:expr,
+        first_pattern = $first_pattern:expr,
+        first_token = $first_tokens:expr,
+        var = $var:tt,
+    ) => {
+        let $var = if regex::Regex::new($first_pattern).unwrap().is_match($str) {
+            $first_tokens
+        } $(else if regex::Regex::new($pattern).unwrap().is_match($str) {
+            $tokens
+        })* else {
+            $default_token
+        };
+    };
+
+    (
+        tokens = [$($pattern:expr => $tokens:expr,)* _ => $default_token:expr$(,)?],
         str = $str:expr,
         first_pattern = $first_pattern:expr,
         first_token = $first_tokens:expr,
@@ -31,20 +63,36 @@ macro_rules! __regexm {
             $first_tokens
         } $(else if regex::Regex::new($pattern).unwrap().is_match($str) {
             $tokens
-        })*
+        })* else {
+            $default_token
+        }
     };
 }
 
-// #[cfg(test)]
-// mod test {
-//     super::regex;
-//     #[test]
-//     fn test_regexm() {
-//         let text = "foo";
-//         let result: &str;
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_regexm() {
+        let text1 = "2021-01-01";
+        regexm!(match text1 {
+            r"^\d{4}-\d{2}-\d{2}$" => assert!(true),
+            r"^\d{4}-\d{2}$" => assert!(false),
+            _ => assert!(false),
+        });
 
-//         regexm!(match text {
-//             r#"foo?"# => result = "foo",
-//         })
-//     }
-// }
+        regexm!(let foo = match text1 {
+            r"^\d{4}-\d{2}-\d{2}$" => "ymd",
+            r"^\d{4}-\d{2}$" => "ym",
+            _ => "default"
+        });
+        assert_eq!(foo, "ymd");
+
+        let text2 = "foo";
+        regexm!(let foo = match text2 {
+            r"^\d{4}-\d{2}-\d{2}$" => "ymd",
+            r"^\d{4}-\d{2}$" => "ym",
+            _ => "default"
+        });
+        assert_eq!(foo, "default")
+    }
+}
