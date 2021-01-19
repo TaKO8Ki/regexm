13e12ef1e5841d0089cca6744ff14adc14240511
Takayuki Maeda <takoyaki0316@gmail.com>
2021-01-19 16:12:51 +0900

    block pattern

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,16 +1,20 @@
 fn main() {
     let text = "2020-01-01";
     regexm::regexm!(match text {
-        r"^\d{4}-\d{2}-\d{2}$" => println!("ymd"),
         r"^\d{4}-\d{2}$" => println!("ym"),
+        r"^\d{4}-\d{2}-\d{2}$" => {
+            println!("ymd");
+            println!("ymd");
+        }
+        r"^\d{4}-\d{2}a$" => println!("ym"),
         _ => println!("default"),
     });
 
-    let text2 = "foo";
-    regexm::regexm!(let foo = match text2 {
-        r"^\d{4}-\d{2}-\d{2}$" => "ymd",
-        r"^\d{4}-\d{2}$" => "ym",
-        _ => "default"
-    });
-    println!("{}", foo)
+    // let text2 = "foo";
+    // regexm::regexm!(let foo = match text2 {
+    //     r"^\d{4}-\d{2}-\d{2}$" => "ymd",
+    //     r"^\d{4}-\d{2}$" => "ym",
+    //     _ => "default"
+    // });
+    // println!("{}", foo)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,20 @@ macro_rules! regexm_parse {
             first_pattern = $pattern,
             first_token = $token,
             var = $var,
+            result = unknown,
+        }
+    };
+
+    (
+        tokens = [let $var:tt = match $str:tt {$pattern:expr => {$token:expr} $($rest:tt)*}],
+    ) => {
+        $crate::__regexm_parse! {
+            tokens = [$($rest)*],
+            str = $str,
+            first_pattern = $pattern,
+            first_token = $token,
+            var = $var,
+            result = unknown,
         }
     };
 
@@ -30,6 +44,19 @@ macro_rules! regexm_parse {
             str = $str,
             first_pattern = $pattern,
             first_token = $token,
+            result = unknown,
+        }
+    };
+
+    (
+        tokens = [match $str:tt {$pattern:expr => $token:block $($rest:tt)*}],
+    ) => {
+        $crate::__regexm_parse! {
+            tokens = [$($rest)*],
+            str = $str,
+            first_pattern = $pattern,
+            first_token = $token,
+            result = unknown,
         }
     };
 }
@@ -37,36 +64,180 @@ macro_rules! regexm_parse {
 #[macro_export]
 #[doc(hidden)]
 macro_rules! __regexm_parse {
+    // (
+    //     tokens = [_ => $default_token:expr$(,)?],
+    //     str = $str:expr,
+    //     first_pattern = $first_pattern:expr,
+    //     first_token = $first_tokens:expr,
+    //     var = $var:tt,
+    //     result = [$result:tt],
+    // ) => {
+    //     let $var = $result else {$default_token};
+    // };
     (
-        tokens = [$($pattern:expr => $tokens:expr,)* _ => $default_token:expr$(,)?],
+        tokens = [_ => $default_token:expr$(,)?],
         str = $str:expr,
         first_pattern = $first_pattern:expr,
         first_token = $first_tokens:expr,
-        var = $var:tt,
+        result = [$($result:tt)*],
     ) => {
-        let $var = if regex::Regex::new($first_pattern).unwrap().is_match($str) {
-            $first_tokens
-        } $(else if regex::Regex::new($pattern).unwrap().is_match($str) {
-            $tokens
-        })* else {
-            $default_token
-        };
+        $($result)*
     };
 
     (
-        tokens = [$($pattern:expr => $tokens:expr,)* _ => $default_token:expr$(,)?],
+        tokens = [_ => $default_token:block],
         str = $str:expr,
         first_pattern = $first_pattern:expr,
         first_token = $first_tokens:expr,
+        result = [$($result:tt)*],
     ) => {
-        if regex::Regex::new($first_pattern).unwrap().is_match($str) {
-            $first_tokens
-        } $(else if regex::Regex::new($pattern).unwrap().is_match($str) {
-            $tokens
-        })* else {
-            $default_token
+        $($result)*
+    };
+
+    // (
+    //     tokens = [$pattern:expr => $token:expr, $($rest:tt)*],
+    //     str = $str:expr,
+    //     first_pattern = $first_pattern:expr,
+    //     first_token = $first_tokens:expr,
+    //     var = $var:tt,
+    //     result = unknown,
+    // ) => {
+    //     $crate::__regexm_parse! {
+    //         tokens = [$($rest)*],
+    //         str = $str,
+    //         first_pattern = $first_pattern,
+    //         first_token = $first_tokens,
+    //         var = $var,
+    //         result = [if regex::Regex::new($first_pattern).unwrap().is_match($str) {
+    //             $first_tokens
+    //         } else if regex::Regex::new($pattern).unwrap().is_match($str) {
+    //             $token
+    //         }],
+    //     }
+    // };
+
+    (
+        tokens = [$pattern:expr => $token:expr, $($rest:tt)*],
+        str = $str:expr,
+        first_pattern = $first_pattern:expr,
+        first_token = $first_tokens:expr,
+        result = unknown,
+    ) => {
+        $crate::__regexm_parse! {
+            tokens = [$($rest)*],
+            str = $str,
+            first_pattern = $first_pattern,
+            first_token = $first_tokens,
+            result = [if regex::Regex::new($first_pattern).unwrap().is_match($str) {
+                $first_tokens
+            } else if regex::Regex::new($pattern).unwrap().is_match($str) {
+                $token
+            }],
         }
     };
+
+    (
+        tokens = [$pattern:expr => $token:block $($rest:tt)*],
+        str = $str:expr,
+        first_pattern = $first_pattern:expr,
+        first_token = $first_tokens:expr,
+        result = unknown,
+    ) => {
+        $crate::__regexm_parse! {
+            tokens = [$($rest)*],
+            str = $str,
+            first_pattern = $first_pattern,
+            first_token = $first_tokens,
+            result = [if regex::Regex::new($first_pattern).unwrap().is_match($str) {
+                $first_tokens
+            } else if regex::Regex::new($pattern).unwrap().is_match($str) {
+                $token
+            }],
+        }
+    };
+
+    (
+        tokens = [$pattern:expr => $token:expr, $($rest:tt)*],
+        str = $str:expr,
+        first_pattern = $first_pattern:expr,
+        first_token = $first_tokens:expr,
+        result = [$($result:tt)*],
+    ) => {
+        $crate::__regexm_parse! {
+            tokens = [$($rest)*],
+            str = $str,
+            first_pattern = $first_pattern,
+            first_token = $first_tokens,
+            result = [$($result)* else if regex::Regex::new($pattern).unwrap().is_match($str) {
+                $token
+            }],
+        }
+    };
+
+    (
+        tokens = [$pattern:expr => $token:block $($rest:tt)*],
+        str = $str:expr,
+        first_pattern = $first_pattern:expr,
+        first_token = $first_tokens:expr,
+        result = [$($result:tt)*],
+    ) => {
+        $crate::__regexm_parse! {
+            tokens = [$($rest)*],
+            str = $str,
+            first_pattern = $first_pattern,
+            first_token = $first_tokens,
+            result = [$($result)* else if regex::Regex::new($pattern).unwrap().is_match($str) {
+                $token
+            }],
+        }
+    };
+
+    // (
+    //     tokens = [$pattern:expr => $token:expr, $($rest:tt)*],
+    //     str = $str:expr,
+    //     first_pattern = $first_pattern:expr,
+    //     first_token = $first_tokens:expr,
+    //     var = $var:tt,
+    //     result = [$result:tt],
+    // ) => {
+    //     $crate::__regexm_parse! {
+    //         tokens = [$($rest)*],
+    //         str = $str,
+    //         first_pattern = $first_pattern,
+    //         first_token = $first_tokens,
+    //         var = $var,
+    //         result = [$result else if regex::Regex::new($pattern).unwrap().is_match($str) {
+    //             $tokens
+    //         }],
+    //     }
+    // };
+
+    // (
+    //     tokens = [$($pattern:expr => $tokens:expr,)* _ => $default_token:expr$(,)?],
+    //     str = $str:expr,
+    //     first_pattern = $first_pattern:expr,
+    //     first_token = $first_tokens:expr,
+    //     var = $var:tt,
+    //     result = [$result:expr],
+    // ) => {
+    //     let $var = $result;
+    // };
+
+    // (
+    //     tokens = [$($pattern:expr => $tokens:expr,)* _ => $default_token:expr$(,)?],
+    //     str = $str:expr,
+    //     first_pattern = $first_pattern:expr,
+    //     first_token = $first_tokens:expr,
+    //     result = $result:tt,
+    // ) => {
+    //     if regex::Regex::new($first_pattern).unwrap().is_match($str) {
+    //         $first_tokens
+    //     } $(else if regex::Regex::new($pattern).unwrap().is_match($str) {
+    //         $tokens
+    //     })* else {
+    //         $default_token
+    //     }
+    // };
 }
 
 #[cfg(test)]
@@ -80,19 +251,19 @@ mod test {
             _ => assert!(false),
         });
 
-        regexm!(let foo = match text1 {
-            r"^\d{4}-\d{2}-\d{2}$" => "ymd",
-            r"^\d{4}-\d{2}$" => "ym",
-            _ => "default"
-        });
-        assert_eq!(foo, "ymd");
+        // regexm!(let foo = match text1 {
+        //     r"^\d{4}-\d{2}-\d{2}$" => "ymd",
+        //     r"^\d{4}-\d{2}$" => "ym",
+        //     _ => "default"
+        // });
+        // assert_eq!(foo, "ymd");
 
-        let text2 = "foo";
-        regexm!(let foo = match text2 {
-            r"^\d{4}-\d{2}-\d{2}$" => "ymd",
-            r"^\d{4}-\d{2}$" => "ym",
-            _ => "default"
-        });
-        assert_eq!(foo, "default")
+        // let text2 = "foo";
+        // regexm!(let foo = match text2 {
+        //     r"^\d{4}-\d{2}-\d{2}$" => "ymd",
+        //     r"^\d{4}-\d{2}$" => "ym",
+        //     _ => "default"
+        // });
+        // assert_eq!(foo, "default")
     }
 }
