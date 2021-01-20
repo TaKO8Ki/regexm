24effff327f8d214590c90cc2153674c07e03e72
Takayuki Maeda <takoyaki0316@gmail.com>
2021-01-21 00:15:29 +0900

    remove `let match =`

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,6 @@
 #[macro_export]
 macro_rules! regexm {
     (
-        let $var:tt = match $str:tt {$pattern:expr => $token:expr, $($rest:tt)*}
-    ) => {
-        $crate::__regexm! {
-            tokens = [$($rest)*],
-            str = $str,
-            first_pattern = $pattern,
-            first_token = $token,
-            var = $var,
-            result = unknown,
-        }
-    };
-
-    (
-        let $var:tt = match $str:tt {$pattern:expr => $token:block $($rest:tt)*}
-    ) => {
-        $crate::__regexm! {
-            tokens = [$($rest)*],
-            str = $str,
-            first_pattern = $pattern,
-            first_token = $token,
-            var = $var,
-            result = unknown,
-        }
-    };
-
-    // match
-    (
         match $str:tt {$pattern:expr => $token:expr, $($rest:tt)*}
     ) => {
         $crate::__regexm! {
@@ -60,183 +33,9 @@ macro_rules! __regexm {
         str = $str:expr,
         first_pattern = $first_pattern:expr,
         first_token = $first_tokens:expr,
-        var = $var:tt,
         result = [$($result:tt)*],
     ) => {
-        let $var = $($result)* else {$default_token};
-    };
-
-    (
-        tokens = [_ => $default_token:block$(,)?],
-        str = $str:expr,
-        first_pattern = $first_pattern:expr,
-        first_token = $first_tokens:expr,
-        var = $var:tt,
-        result = [$($result:tt)*],
-    ) => {
-        let $var = $($result)* else {$default_token};
-    };
-
-    (
-        tokens = [_ => $default_token:expr$(,)?],
-        str = $str:expr,
-        first_pattern = $first_pattern:expr,
-        first_token = $first_tokens:expr,
-        var = $var:tt,
-        result = unknown,
-    ) => {
-        let $var = if regex::Regex::new($first_pattern).unwrap().is_match($str) {
-            $first_tokens
-        } else {$default_token};
-    };
-
-    (
-        tokens = [_ => $default_token:block$(,)?],
-        str = $str:expr,
-        first_pattern = $first_pattern:expr,
-        first_token = $first_tokens:expr,
-        var = $var:tt,
-        result = unknown,
-    ) => {
-        let $var = if regex::Regex::new($first_pattern).unwrap().is_match($str) {
-            $first_tokens
-        } else {$default_token};
-    };
-
-    (
-        tokens = [$pattern:expr => $token:block, $($rest:tt)*],
-        str = $str:expr,
-        first_pattern = $first_pattern:expr,
-        first_token = $first_tokens:expr,
-        var = $var:tt,
-        result = unknown,
-    ) => {
-        $crate::__regexm! {
-            tokens = [$($rest)*],
-            str = $str,
-            first_pattern = $first_pattern,
-            first_token = $first_tokens,
-            var = $var,
-            result = [if regex::Regex::new($first_pattern).unwrap().is_match($str) {
-                $first_tokens
-            } else if regex::Regex::new($pattern).unwrap().is_match($str) {
-                $token
-            }],
-        }
-    };
-
-    (
-        tokens = [$pattern:expr => $token:block $($rest:tt)*],
-        str = $str:expr,
-        first_pattern = $first_pattern:expr,
-        first_token = $first_tokens:expr,
-        var = $var:tt,
-        result = unknown,
-    ) => {
-        $crate::__regexm! {
-            tokens = [$($rest)*],
-            str = $str,
-            first_pattern = $first_pattern,
-            first_token = $first_tokens,
-            var = $var,
-            result = [if regex::Regex::new($first_pattern).unwrap().is_match($str) {
-                $first_tokens
-            } else if regex::Regex::new($pattern).unwrap().is_match($str) {
-                $token
-            }],
-        }
-    };
-
-    (
-        tokens = [$pattern:expr => $token:expr, $($rest:tt)*],
-        str = $str:expr,
-        first_pattern = $first_pattern:expr,
-        first_token = $first_tokens:expr,
-        var = $var:tt,
-        result = unknown,
-    ) => {
-        $crate::__regexm! {
-            tokens = [$($rest)*],
-            str = $str,
-            first_pattern = $first_pattern,
-            first_token = $first_tokens,
-            var = $var,
-            result = [if regex::Regex::new($first_pattern).unwrap().is_match($str) {
-                $first_tokens
-            } else if regex::Regex::new($pattern).unwrap().is_match($str) {
-                $token
-            }],
-        }
-    };
-
-    (
-        tokens = [$pattern:expr => $token:expr, $($rest:tt)*],
-        str = $str:expr,
-        first_pattern = $first_pattern:expr,
-        first_token = $first_tokens:expr,
-        var = $var:tt,
-        result = [$($result:tt)*],
-    ) => {
-        $crate::__regexm! {
-            tokens = [$($rest)*],
-            str = $str,
-            first_pattern = $first_pattern,
-            first_token = $first_tokens,
-            var = $var,
-            result = [$($result)* else if regex::Regex::new($pattern).unwrap().is_match($str) {
-                $token
-            }],
-        }
-    };
-
-    (
-        tokens = [$pattern:expr => $token:block, $($rest:tt)*],
-        str = $str:expr,
-        first_pattern = $first_pattern:expr,
-        first_token = $first_tokens:expr,
-        var = $var:tt,
-        result = [$($result:tt)*],
-    ) => {
-        $crate::__regexm! {
-            tokens = [$($rest)*],
-            str = $str,
-            first_pattern = $first_pattern,
-            first_token = $first_tokens,
-            var = $var,
-            result = [$($result)* else if regex::Regex::new($pattern).unwrap().is_match($str) {
-                $token
-            }],
-        }
-    };
-
-    (
-        tokens = [$pattern:expr => $token:block $($rest:tt)*],
-        str = $str:expr,
-        first_pattern = $first_pattern:expr,
-        first_token = $first_tokens:expr,
-        var = $var:tt,
-        result = [$($result:tt)*],
-    ) => {
-        $crate::__regexm! {
-            tokens = [$($rest)*],
-            str = $str,
-            first_pattern = $first_pattern,
-            first_token = $first_tokens,
-            var = $var,
-            result = [$($result)* else if regex::Regex::new($pattern).unwrap().is_match($str) {
-                $token
-            }],
-        }
-    };
-
-    (
-        tokens = [_ => $default_token:expr$(,)?],
-        str = $str:expr,
-        first_pattern = $first_pattern:expr,
-        first_token = $first_tokens:expr,
-        result = [$($result:tt)*],
-    ) => {
-        $($result)*
+        $($result)* else {$default_token};
     };
 
     (
@@ -246,7 +45,7 @@ macro_rules! __regexm {
         first_token = $first_tokens:expr,
         result = [$($result:tt)*],
     ) => {
-        $($result)*
+        $($result)* else {$default_token};
     };
 
     (
@@ -258,9 +57,7 @@ macro_rules! __regexm {
     ) => {
         if regex::Regex::new($first_pattern).unwrap().is_match($str) {
             $first_tokens
-        } else {
-            $default_token
-        }
+        } else {$default_token};
     };
 
     (
@@ -272,29 +69,7 @@ macro_rules! __regexm {
     ) => {
         if regex::Regex::new($first_pattern).unwrap().is_match($str) {
             $first_tokens
-        } else {
-            $default_token
-        }
-    };
-
-    (
-        tokens = [$pattern:expr => $token:expr, $($rest:tt)*],
-        str = $str:expr,
-        first_pattern = $first_pattern:expr,
-        first_token = $first_tokens:expr,
-        result = unknown,
-    ) => {
-        $crate::__regexm! {
-            tokens = [$($rest)*],
-            str = $str,
-            first_pattern = $first_pattern,
-            first_token = $first_tokens,
-            result = [if regex::Regex::new($first_pattern).unwrap().is_match($str) {
-                $first_tokens
-            } else if regex::Regex::new($pattern).unwrap().is_match($str) {
-                $token
-            }],
-        }
+        } else {$default_token};
     };
 
     (
@@ -319,6 +94,26 @@ macro_rules! __regexm {
 
     (
         tokens = [$pattern:expr => $token:block $($rest:tt)*],
+        str = $str:expr,
+        first_pattern = $first_pattern:expr,
+        first_token = $first_tokens:expr,
+        result = unknown,
+    ) => {
+        $crate::__regexm! {
+            tokens = [$($rest)*],
+            str = $str,
+            first_pattern = $first_pattern,
+            first_token = $first_tokens,
+            result = [if regex::Regex::new($first_pattern).unwrap().is_match($str) {
+                $first_tokens
+            } else if regex::Regex::new($pattern).unwrap().is_match($str) {
+                $token
+            }],
+        }
+    };
+
+    (
+        tokens = [$pattern:expr => $token:expr, $($rest:tt)*],
         str = $str:expr,
         first_pattern = $first_pattern:expr,
         first_token = $first_tokens:expr,
@@ -440,7 +235,7 @@ mod test {
             }
             r"^\d{4}-\d{2}$" => {
                 assert!(false);
-                assert!(false)
+                assert!(false);
             }
             _ => {
                 assert!(true);
@@ -508,21 +303,21 @@ mod test {
 
     #[test]
     fn test_let_match_3_or_more_pattern() {
-        regexm!(let foo = match "2021-01-01" {
+        let foo = regexm!(match "2021-01-01" {
             r"^\d{4}-\d{2}-\d{2}$" => "ymd",
             r"^\d{4}-\d{2}$" => "ym",
-            _ => "default"
+            _ => "default",
         });
         assert_eq!(foo, "ymd");
 
-        regexm!(let foo = match "foo" {
+        let foo = regexm!(match "foo" {
             r"^\d{4}-\d{2}-\d{2}$" => "ymd",
             r"^\d{4}-\d{2}$" => "ym",
-            _ => "default"
+            _ => "default",
         });
         assert_eq!(foo, "default");
 
-        regexm!(let foo = match "2021-01-01" {
+        let foo = regexm!(match "2021-01-01" {
             r"^\d{4}-\d{2}-\d{2}$" => "ymd",
             r"^\d{4}-\d{2}$" => "ym",
             r"^\d{4}$" => "y",
@@ -533,20 +328,20 @@ mod test {
 
     #[test]
     fn test_let_match_3_or_more_pattern_block() {
-        regexm!(let foo = match "2021-01-01" {
+        let foo = regexm!(match "2021-01-01" {
             r"^\d{4}-\d{2}-\d{2}$" => {
                 let bar = "ymd";
                 bar
-            },
+            }
             r"^\d{4}-\d{2}$" => {
                 let bar = "ym";
                 bar
             }
-            _ => "default"
+            _ => "default",
         });
         assert_eq!(foo, "ymd");
 
-        regexm!(let foo = match "foo" {
+        let foo = regexm!(match "foo" {
             r"^\d{4}-\d{2}-\d{2}$" => {
                 let bar = "ymd";
                 bar
@@ -554,16 +349,16 @@ mod test {
             r"^\d{4}-\d{2}$" => {
                 let bar = "ym";
                 bar
-            },
-            _ => "default"
+            }
+            _ => "default",
         });
         assert_eq!(foo, "default");
 
-        regexm!(let foo = match "2021-01-01" {
+        let foo = regexm!(match "2021-01-01" {
             r"^\d{4}-\d{2}-\d{2}$" => {
                 let bar = "ymd";
                 bar
-            },
+            }
             r"^\d{4}-\d{2}$" => {
                 let bar = "ym";
                 bar
@@ -571,44 +366,44 @@ mod test {
             r"^\d{4}$" => {
                 let bar = "y";
                 bar
-            },
-            _ => "default"
+            }
+            _ => "default",
         });
         assert_eq!(foo, "ymd");
     }
 
     #[test]
     fn test_let_match_2_or_less_pattern() {
-        regexm!(let foo = match "2021-01-01" {
+        let foo = regexm!(match "2021-01-01" {
             r"^\d{4}-\d{2}-\d{2}$" => "ymd",
-            _ => "default"
+            _ => "default",
         });
         assert_eq!(foo, "ymd");
 
-        regexm!(let foo = match "foo" {
+        let foo = regexm!(match "foo" {
             r"^\d{4}-\d{2}$" => "ym",
-            _ => "default"
+            _ => "default",
         });
         assert_eq!(foo, "default");
     }
 
     #[test]
     fn test_let_match_2_or_less_pattern_block() {
-        regexm!(let foo = match "2021-01-01" {
+        let foo = regexm!(match "2021-01-01" {
             r"^\d{4}-\d{2}-\d{2}$" => {
                 let bar = "ymd";
                 bar
             }
-            _ => "default"
+            _ => "default",
         });
         assert_eq!(foo, "ymd");
 
-        regexm!(let foo = match "foo" {
+        let foo = regexm!(match "foo" {
             r"^\d{4}-\d{2}$" => {
                 let bar = "ym";
                 bar
             }
-            _ => "default"
+            _ => "default",
         });
         assert_eq!(foo, "default");
     }
