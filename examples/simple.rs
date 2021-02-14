0bf4f646f8a51d6d4f9c6c5847566172ea928794
Takayuki Maeda <takoyaki0316@gmail.com>
2021-02-14 14:48:29 +0900

    fix date format

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,26 +1,26 @@
 fn main() {
     let text1 = "2020-01-01";
     regexm::regexm!(match text1 {
-        r"^\d{4}$" => println!("y"),
-        r"^\d{4}-\d{2}$" => println!("y-m"),
+        r"^\d{4}$" => println!("yyyy"),
+        r"^\d{4}-\d{2}$" => println!("yyyy-mm"),
         // block
         r"^\d{4}-\d{2}-\d{2}$" => {
-            let y_m_d = "y-m-d";
-            println!("{}", y_m_d);
+            let yyyy_mm_dd = "yyyy-mm-dd";
+            println!("{}", yyyy_mm_dd);
         }
         _ => println!("default"),
     });
 
-    // Output: y-m-d
+    // Output: yyyy-mm-dd
 
     let text2 = "foo";
     let foo = regexm::regexm!(match text2 {
-        r"^\d{4}-\d{2}-\d{2}$" => "ymd",
-        r"^\d{4}-\d{2}$" => "ym",
+        r"^\d{4}-\d{2}-\d{2}$" => "yyyy-mm-dd",
+        r"^\d{4}-\d{2}$" => "yyyy-mm",
         // block
         r"^\d{4}-\d{2}-\d{2}$" => {
-            let ymd = "ymd";
-            ymd
+            let yyyy_mm_dd = "yyyy-mm-dd";
+            yyyy_mm_dd
         }
         _ => "default",
     });
